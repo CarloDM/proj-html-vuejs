1b74d388e60fd305477db5ef2dbb5e13870627d9
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-09 18:37:23 +0200

    subscribe layout

--- src/components/partials/cta-subscribe.vue
+++ src/components/partials/cta-subscribe.vue
@@ -1,15 +1,44 @@
 <script>
 export default {
-  name:'jumbotron',
+  name:'cta subscribe',
   
 }
 </script>
 
 <template>
-  
+  <div class="cta_subscribe">
+    <div class="my_container debug">
+
+      <div class="container">
+        <h2>Subscribe</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil quasi eveniet odit sunt.</p>
+
+        <input type="text">
+        <button>SUBSCRIBE</button>
+
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+
+    <div class="foto_container"></div>
+
+  </div>
 </template>
 
 
-<style>
+<style lang="scss"  scoped>
+
+.cta_subscribe{
+  background-image: url(../../assets/img/subscribe-sec-bg.png);
+  background-size: cover;
+  .my_container{
+
+    .container{
+      text-align: center;
+      width: 90%;
+      margin: 40px auto;
+    }
+  }
+}
 
 </style>
