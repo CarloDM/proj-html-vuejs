77cc63df26c94af1acdf106f2390b768e514ed7a
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-11 17:54:50 +0200

    scss photo hover

--- src/components/singleComponents/photo.vue
+++ src/components/singleComponents/photo.vue
@@ -2,25 +2,51 @@
 export default {
   name:'photo',
   props:{
-    imgUrl:String,
+    imgUrl: String,
+    title: String,
   }
 }
 </script>
 
 <template>
-  <div class="photo flex_center">
-    <!-- <img src="../../assets/img/food/fi-korean-food-400x263.jpg" alt=""> -->
+  <div class="photo flex_center hover_photo">
+
     <img :src=imgUrl alt="">
+
+    <p class="link"><a class="clr_white" href="#">link</a></p>
+    <p class="title">{{title}}</p>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../../scss/var.scss' as * ;
 .photo{
   width: 100%;
-  
+  flex-direction: column;
+  background-image: linear-gradient($black,$avada_orange);
   img{
+    transition: all 0.2s ease;
     width: 100%;
+    opacity: 1;
+  }
+  .link,.title{
+    transition: all 0.2s ease;
+    position: absolute;
+    opacity: 0;
+  }
+  .title{
+    transform: translateY(100%);
+  }
+
+  &:hover{
+    img{
+      opacity: 0.1;
+    }
+    .link,.title{
+      display: block;
+      opacity: 1;
+    }
   }
 }
 </style>
